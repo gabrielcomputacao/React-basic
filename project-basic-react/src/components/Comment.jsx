611ad1c22357cad01f83c2377930ecbe7e88098e
gabriel <gabrielcomputacao@outlook.com>
2023-09-24 18:13:19 -0300

    feat: criando componente de comentário

---
+++ project-basic-react/src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gabrielcomputacao.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Henrique da Silveira Santos</strong>
+              <time title="2023-09-22 7:00" dateTime="2023-09-22 7:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            {/* quadno o botao nao tiver texto e sim um icone e sempre bom colocar um title para os leitores de tela */}
+            <button title="Deletar Comentário" >
+                <Trash  size={24}/>
+            </button>
+
+          </header>
+
+          <p>Muito Bom!</p>
+        </div>
+
+        <footer>
+
+            <button>
+                <ThumbsUp />
+                Aplaudir <span>20</span>
+            </button>
+
+        </footer>
+      </div>
+    </div>
+  );
+}
--- project-basic-react/src/components/Post.jsx
+++ project-basic-react/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -48,6 +49,14 @@ export function Post() {
         </footer>
         
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+
+
     </article>
   );
 }
