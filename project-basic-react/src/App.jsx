aafa11e3ac8d1444805e523edc727816f15baf63
gabriel <gabrielcomputacao@outlook.com>
2023-09-26 07:17:09 -0300

    feat: iterando posts na tela principal

--- project-basic-react/src/App.jsx
+++ project-basic-react/src/App.jsx
@@ -4,6 +4,42 @@ import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+//Data of Application
+//author: {url_avatar: '', name: '', role:''}
+//publishedAt: Date
+//content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/gabrielcomputacao.png",
+      name: "Gabriel Henrique",
+      role: "Software Developer",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera!'} ,
+      {type: 'paragraph', content: 'Acabei de subir meu novo projeto'},
+      {type: 'link', content: 'https://github.com/gabrielcomputacao/'},
+    ],
+    publishAt: new Date('2023-09-26 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/gabrielcomputacao.png",
+      name: "Igor Santos",
+      role: "Programador Junior",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera eu sou o Igor!'} ,
+      {type: 'paragraph', content: 'Acabei de subir minha nova tarefa.'},
+      {type: 'link', content: 'https://github.com/gabrielcomputacao/'},
+    ],
+    publishAt: new Date('2023-09-15 20:00:00')
+  },
+];
+
 function App() {
   return (
     <>
@@ -11,7 +47,10 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          
+        {
+          posts.map( (post,index) => <Post key={index} author={post.author} content={post.content} publishedAt={post.publishAt} /> )
+        }
         </main>
       </div>
     </>
