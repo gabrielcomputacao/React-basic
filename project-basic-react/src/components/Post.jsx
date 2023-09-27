0d6b978bc5b8392132c2e891dabfd472e555ee74
gabriel <gabrielcomputacao@outlook.com>
2023-09-26 21:39:51 -0300

    feat: formatando data Post e incluindo props

--- project-basic-react/src/components/Post.jsx
+++ project-basic-react/src/components/Post.jsx
@@ -1,49 +1,61 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
+export function Post({ author, publishedAt, content}) {
+
+  const [comments, setComments] = useState([1,2]);
 
 
-export function Post(props) {
+  function handleCreateNewComment(){
+    event.preventDefault();
+    
+    setComments([...comments, comments.length + 1 ]);
 
-  console.log(props)
+  }
+
+  const publisehdDateFormatted = new Intl.DateTimeFormat( 'pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    hour: '2-digit',
+    minute: '2-digit', 
+  } ).format(publishedAt);
 
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={"https://github.com/gabrielcomputacao.png"} />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong> Gabriel Henrique</strong>
-            <span> Web Developer </span>
+            <strong> {author.name}</strong>
+            <span> {author.role} </span>
           </div>
         </div>
 
         {/* tag time do html permite passar um atributo para ele para manusear datas */}
-        <time title="2023-09-22 7:00" dateTime="2023-09-22 7:00">
+        <time title={publisehdDateFormatted} dateTime={publishedAt.toISOString()}>
           Publicado há 1h
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a
-          quisquam quidem minus ad numquam reiciendis! Nisi aspernatur ad
-          voluptas dolore ipsa quis. Fugit nulla perspiciatis fuga voluptates
-          deserunt necessitatibus!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, nulla
-          quaerat tempore deleniti, odio, ea dolorum cumque sunt necessitatibus
-          quidem voluptatibus distinctio asperiores minus nisi temporibus a.
-          Non, repudiandae dicta.
-        </p>
-        <p>
-          <a href="#">Link que leva para a pagina inicial.</a>
-        </p>
+        {
+          content.map( (line, index) => {
+            if(line.type == 'paragraph'){
+              return <p key={index}> {line.content} </p>
+            }else{
+              return <p key={index}>   <a href={ line.content }> Link Para o Projeto no GitHub </a> </p>
+            }
+          } )
+        }
+       
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu Feedback</strong>
 
         <textarea placeholder="Deixa um comentário" />
@@ -53,9 +65,9 @@ export function Post(props) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+       {
+        comments.map( (_, index) => <Comment key={index} /> )
+       }
       </div>
     </article>
   );
